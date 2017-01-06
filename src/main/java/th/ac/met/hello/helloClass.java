feb756d96093b174f7454a4cc220a79eab589bfb
SoponThaiou <soponthaiou@gmail.com>
2017-01-06 21:50:20 +0700

    Add package Hello

---
+++ src/main/java/th/ac/met/hello/helloClass.java
@@ -0,0 +1,10 @@
+package th.ac.met.hello;
+
+/**
+ * Created by Admin on 6/1/2560.
+ */
+public class helloClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
